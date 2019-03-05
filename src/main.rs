a805fa03a54490b5f65ea8a9a599237e8ca11b8e
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-03-05 20:17:30 +0300

    Registered post views

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate diesel;
+
+extern crate serde;
+extern crate dotenv;
+
+mod models;
+mod views;
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .register(catchers![views::not_found])
+        .mount("/",
+               routes![views::new_post, views::get_post, views::get_posts])
+        .launch();
 }

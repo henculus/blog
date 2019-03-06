f28f205c5d94b6da0612be34a8c09b9ac45cc427
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-03-06 19:58:46 +0300

    Created update and delete methods

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -3,15 +3,14 @@ mod errors;
 use rocket::request::Form;
 use rocket_contrib::json::Json;
 use crate::models::post::*;
-use crate::models::connection;
-use crate::models::Id;
-use rocket::Request;
+use crate::models::{Id, DBPool};
+use rocket::{Request, State};
 use crate::views::errors::NotFoundError;
 
 #[post("/posts", format = "application/json", data = "<post>")]
-pub fn new_post(post: Json<NewPost>) -> Option<Json<Post>> {
-    let connection = connection();
-    let posts_table = PostsTable::instance(connection);
+pub fn new_post(post: Json<NewPost>, db: State<DBPool>) -> Option<Json<Post>> {
+    let connection = db.get().unwrap();
+    let posts_table = PostsTable::instance(&*connection);
     posts_table
         .create(post.into_inner())
         .map(|post| Json(post))
@@ -25,22 +24,42 @@ pub struct LimitOffset {
 }
 
 #[get("/posts?<cursor..>")]
-pub fn get_posts(offlim: Form<LimitOffset>) -> Option<Json<Vec<Post>>> {
-    let connection = connection();
-    let posts_table = PostsTable::instance(connection);
+pub fn get_posts(cursor: Form<LimitOffset>, db: State<DBPool>) -> Option<Json<Vec<Post>>> {
+    let connection = db.get().unwrap();
+    let posts_table = PostsTable::instance(&*connection);
     posts_table
-        .get(offlim.limit.unwrap_or(1), offlim.offset.unwrap_or(0))
+        .get(cursor.limit.unwrap_or(1), cursor.offset.unwrap_or(0))
         .map(|posts| Json(posts))
         .ok()
 }
 
 #[get("/posts/<id>")]
-pub fn get_post(id: Id) -> Option<Json<Post>> {
-    let connection = connection();
-    let posts_table = PostsTable::instance(connection);
+pub fn get_post(id: Id, db: State<DBPool>) -> Option<Json<Post>> {
+    let connection = db.get().unwrap();
+    let posts_table = PostsTable::instance(&*connection);
     posts_table
         .get_by_id(id)
         .map(|post| Json(post))
+        .ok()
+}
+
+#[put("/posts/<id>", format = "application/json", data = "<post>")]
+pub fn update_post(id: Id, post: Json<NewPost>, db: State<DBPool>) -> Option<Json<Id>> {
+    let connection = db.get().unwrap();
+    let posts_table = PostsTable::instance(&*connection);
+    posts_table
+        .update(id, post.into_inner())
+        .map(|id| Json(id))
+        .ok()
+}
+
+#[delete("/posts/<id>")]
+pub fn delete_post(id: Id, db: State<DBPool>) -> Option<Json<Id>> {
+    let connection = db.get().unwrap();
+    let posts_table = PostsTable::instance(&*connection);
+    posts_table
+        .delete(id)
+        .map(|id| Json(id))
         .ok()
 }
 

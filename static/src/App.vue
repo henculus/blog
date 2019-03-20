21c0c63f6db0546c78f256e45066953729dbcc7c
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-03-20 17:31:44 +0300

    Created menu bar and footer

--- static/src/App.vue
+++ static/src/App.vue
@@ -1,20 +1,33 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <MenuBar/>
+        <router-view/>
+        <footer>
+            <p>Created by LupusAnay</p>
+            <p>Contact information: <a href="mailto:lupusanay@gmail.com">
+                lupusanay@gmail.com</a>.</p>
+        </footer>
     </div>
 </template>
 
 <script>
-    export default {}
+    import MenuBar from "@/components/MenuBar";
+
+    export default {
+        components: {MenuBar}
+    }
 </script>
 
 <style>
+    body {
+        margin: 0;
+        background-color: #F0E6EF;
+    }
+
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
     }
 </style>
---
+++ static/src/components/MenuBar.vue
@@ -0,0 +1,73 @@
+<template>
+    <div id="nav-wrapper">
+        <nav id="nav">
+            <router-link class='nav-item'
+                         v-for="routes in links"
+                         v-bind:key="routes.id"
+                         :to="`${routes.page}`">
+                <p class="link-text">{{routes.text}}</p>
+            </router-link>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MenuBar",
+        data() {
+            return {
+                links: [
+                    {
+                        id: 0,
+                        text: 'Rusty Blog',
+                        page: '/'
+                    },
+                    {
+                        id: 1,
+                        text: 'About',
+                        page: '/about'
+                    },
+                    {
+                        id: 2,
+                        text: 'Search',
+                        page: '/search'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    #nav-wrapper {
+        margin-bottom: 4rem;
+        background-color: #9C89B8;
+        display: flex;
+        align-items: stretch;
+        justify-content: center;
+        min-height: 4rem;
+    }
+
+    #nav {
+        display: flex;
+        flex-direction: row;
+        justify-content: flex-start;
+        align-items: stretch;
+        width: 60rem;
+    }
+
+    .nav-item {
+        font-size: 24px;
+        text-decoration: none;
+        color: #F0E6EF;
+    }
+
+    .nav-item:hover {
+        background-color: #EFC3E6;
+        color: #B8BEDD;
+    }
+
+    .link-text {
+        margin: 1rem
+    }
+</style>

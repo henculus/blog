b29ed134e28f284e21e431438ece95279c0aab86
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-07-24 11:01:41 +0300

    Fixed undefined url error in production

--- static/src/http-common.js
+++ static/src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const HTTP = axios.create({
     // Empty when in production, which means that publicPath used
-    baseURL: `${process.env.VUE_APP_API_LOCATION}/api`
+    baseURL: `${process.env.VUE_APP_API_LOCATION || ''}/api`
 });
 
 HTTP.defaults.headers.post['Content-Type'] = 'application/json';

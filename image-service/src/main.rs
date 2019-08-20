c8130c2b4b4a7f46a0e58d92a1b260129d4ae107
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-08-20 10:08:18 +0300

    Logic divided to different functions
    #102

--- image-service/src/main.rs
+++ image-service/src/main.rs
@@ -6,46 +6,73 @@ use actix_web::error::ErrorInternalServerError;
 use actix_web::http::{HeaderValue, StatusCode};
 use actix_web::http::header::CONTENT_TYPE;
 use futures::{Future, Stream};
-use futures::future::{FutureResult, ok};
-use image::FilterType;
+use futures::future::{FutureResult, ok, err};
+use image::{DynamicImage, FilterType};
 use image::ImageFormat::JPEG;
 use log::*;
 
-pub fn resize_img(field: Field) -> impl Future<Item=Vec<u8>, Error=Error> {
+fn get_buffer_from_field(field: Field) -> impl Future<Item=Vec<u8>, Error=Error> {
     field
         .fold(vec![], move |mut acc, bytes| {
             info!("Reading buffer from multipart...");
             acc.extend(bytes);
             ok(acc) as FutureResult<Vec<u8>, MultipartError>
         })
-        .map(|acc| {
-            info!("Loading image from buffer...");
-            let img = image::load_from_memory(&acc).unwrap();
-            info!("Resizing image...");
-            let resized_img = img.resize(100, 100, FilterType::Nearest);
-            let mut buffer = Vec::new();
-            info!("Writing image to buffer...");
-            resized_img.write_to(&mut buffer, JPEG).unwrap();
-            buffer
-        })
         .map_err(|e| {
-            error!("Resizing failed: {:?}", e);
+            error!("Reading multipart failed: {:?}", e);
             ErrorInternalServerError(e)
         })
+}
+
+fn get_image_from_buffer(buffer: Vec<u8>) -> impl Future<Item=DynamicImage, Error=Error> {
+    info!("Loading image from buffer...");
+    match image::load_from_memory(&buffer) {
+        Ok(img) => ok(img),
+        Err(e) => {
+            error!("Image error: {:?}", e);
+            err(ErrorInternalServerError(e))
+        }
+    }
+}
+
+fn resize_img(img: DynamicImage) -> impl Future<Item=DynamicImage, Error=Error> {
+    web::block(move || {
+        info!("Starting resizing image...");
+        Ok(img.resize(100, 100, FilterType::Nearest)) as Result<DynamicImage, ()>
+    })
+        .map_err(|e| {
+            error!("Blocking error: {:?}", e);
+            ErrorInternalServerError(e)
+        })
+}
+
+fn load_img_to_buffer(img: DynamicImage) -> impl Future<Item=Vec<u8>, Error=Error> {
+    let mut buffer = Vec::new();
+    info!("Writing image to buffer...");
+    match img.write_to(&mut buffer, JPEG) {
+        Ok(_) => ok(buffer),
+        Err(e) => {
+            error!("Image error: {:?}", e);
+            err(ErrorInternalServerError(e))
+        }
+    }
+}
+
+pub fn process(field: Field) -> impl Future<Item=Vec<u8>, Error=Error> {
+    get_buffer_from_field(field)
+        .and_then(get_image_from_buffer)
+        .and_then(resize_img)
+        .and_then(load_img_to_buffer)
 }
 
 pub fn upload(multipart: Multipart) -> impl Future<Item=HttpResponse, Error=Error> {
     info!("Started image processing...");
     multipart
-        .map(|field| resize_img(field).into_stream())
+        .map(|field| process(field).into_stream())
         .flatten()
         .collect()
         .map(|buf| {
-            let img: Vec<u8> = buf
-                .iter()
-                .cloned()
-                .flatten()
-                .collect();
+            let img: Vec<u8> = buf.iter().cloned().flatten().collect();
             info!("Writing buffer to response...");
             let mut response = HttpResponse::with_body(StatusCode::OK, img.into());
             response
@@ -74,7 +101,10 @@ fn index() -> HttpResponse {
 }
 
 fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "image_service=debug,actix_server=info,actix_web=info");
+//    std::env::set_var(
+//        "RUST_LOG",
+//        "image_service=debug,actix_server=info,actix_web=info",
+//    );
     env_logger::init();
 
     HttpServer::new(|| {

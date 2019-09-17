317eefee29bd7cbaa35508346d83dc3b02fb89ea
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-09-17 12:56:41 +0300

    Connected views

--- image-service/src/main.rs
+++ image-service/src/main.rs
@@ -1,14 +1,20 @@
+use crate::views::{process_image, index};
 use actix_web::{web, App, HttpServer};
 
-mod img;
 mod error;
+mod img;
 mod imgur_adapter;
 mod multipart_reader;
+mod views;
 
 fn main() -> std::io::Result<()> {
     env_logger::init();
 
-    HttpServer::new(|| App::new().service(web::resource("/")))
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/image").route(web::post().to_async(process_image)))
+            .service(web::resource("/").route(web::get().to(index)))
+    })
         .bind("127.0.0.1:8080")?
         .run()
 }

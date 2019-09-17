07cd00d5993c7b20169fd830874d8966d1057845
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-09-17 12:08:40 +0300

    Added support for multipart error

--- image-service/src/error.rs
+++ image-service/src/error.rs
@@ -1,17 +1,20 @@
 use image::ImageError;
 use reqwest::Error as ReqwestError;
+use actix_multipart::MultipartError;
 
 #[derive(Debug)]
 pub enum Error {
     ImageError(ImageError),
     ReqwestError(ReqwestError),
+    MultipartError(MultipartError),
 }
 
 impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             Error::ImageError(e) => Some(e),
-            Error::ReqwestError(e) => Some(e)
+            Error::ReqwestError(e) => Some(e),
+            Error::MultipartError(e) => None,
         }
     }
 }
@@ -20,7 +23,8 @@ impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             Error::ImageError(e) => e.fmt(f),
-            Error::ReqwestError(e) => e.fmt(f)
+            Error::ReqwestError(e) => e.fmt(f),
+            Error::MultipartError(e) => e.fmt(f),
         }
     }
 }
@@ -34,5 +38,11 @@ impl From<ImageError> for Error {
 impl From<ReqwestError> for Error {
     fn from(reqwest_error: ReqwestError) -> Self {
         Error::ReqwestError(reqwest_error)
+    }
+}
+
+impl From<MultipartError> for Error {
+    fn from(multipart_error: MultipartError) -> Self {
+        Error::MultipartError(multipart_error)
     }
 }

f7c89b98bebe32f0ea883b9efb661ab1723c0eae
RanetkaGod <artyom.serikbaev@gmail.com>
2019-10-15 22:32:36 +0300

    Refactor registration module

--- frontend/src/storage_modules/Authorization.js
+++ frontend/src/storage_modules/Authorization.js
@@ -23,6 +23,9 @@ export const moduleAuthorization = {
         },
         SET_ERROR_MESSAGE(state, payload) {
             state.errorMessage = payload
+        },
+        CLEAR_ERROR_MESSAGE(state) {
+            state.errorMessage = ''
         }
     },
     actions: {
@@ -58,13 +61,13 @@ export const moduleAuthorization = {
                                     dispatch('ModalStore/HideModal', '', {root: true})
                                 },
                                 error => {
-                                    console.log('Error creating session: ', error)
+                                    console.error('Error creating session: ', error)
                                 })
                         }
                     },
                     error => {
                         commit('SET_ERROR_MESSAGE', 'Ошибка входа')
-                        console.log('Ошибка входа: ', error)
+                        console.error('Ошибка входа: ', error)
                         if (error.response) {
                             if (error.response.status === 404) {
                                 commit('SET_ERROR_MESSAGE', 'Такого пользователя нет')
@@ -78,26 +81,51 @@ export const moduleAuthorization = {
                 ).catch(
                     error => {
                         commit('SET_ERROR_MESSAGE', 'Сервер не доступен')
-                        console.log('Ошибка сервера: ', error)
+                        console.error('Ошибка сервера: ', error)
                     }
                 ).finally(() => {
                     commit('END_LOADING')
+                    commit('CLEAR_ERROR_MESSAGE')
                 })
             }
         },
-        registration: function (undefined, payload) {
-            return new Promise((resolve, reject) => {
+        registration: function ({commit, dispatch, getters}, payload) {
+            if (!getters.isLoading) {
+                commit('START_LOADING')
                 api.registration(payload)
                     .then(
                         response => {
                             if (response.status === 200) {
-                                resolve(response)
+                                api.getSession().then(
+                                    response => {
+                                        commit('SET_SESSION', response.data)
+                                        dispatch('ModalStore/HideModal', '', {root: true})
+                                    },
+                                    error => {
+                                        console.error('Error creating session: ', error)
+                                    })
                             }
-                        })
-                    .catch(error => {
-                        reject(error)
-                    })
-            })
+                        },
+                        error => {
+                            if (error.response) {
+                                if (error.response.status === 409) {
+                                    commit('SET_ERROR_MESSAGE', 'Такой пользователь уже есть')
+                                } else {
+                                    commit('SET_ERROR_MESSAGE', 'Ошибка регистрации')
+                                }
+                                console.error('Ошибка при регистрации: ', error.response)
+                            } else {
+                                commit('SET_ERROR_MESSAGE', 'Ошибка сервера')
+                            }
+                        }
+                    ).catch(error => {
+                    commit('SET_ERROR_MESSAGE', 'Ошибка сервера')
+                    console.error('Ошибка при регистрации: ', error)
+                }).finally(() => {
+                    commit('END_LOADING')
+                    commit('CLEAR_ERROR_MESSAGE')
+                })
+            }
         },
         logout: function () {
             return new Promise((resolve, reject) => {

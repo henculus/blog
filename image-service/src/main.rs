c31a463d3154e62539a380f79be45672f1cf77e8
SarielAki <sariely@yandex.ru>
2019-11-09 22:03:42 +0300

    Add CORS to image service

--- image-service/src/main.rs
+++ image-service/src/main.rs
@@ -1,5 +1,6 @@
 use crate::views::{index, process_image};
-use actix_web::{web, App, HttpServer};
+use actix_web::{web, App, HttpServer, http};
+use actix_cors::Cors;
 
 mod error;
 mod img;
@@ -11,11 +12,27 @@ fn main() -> std::io::Result<()> {
     env_logger::init();
 
     HttpServer::new(|| {
-        App::new().service(
-            web::resource("/image")
-                .route(web::post().to_async(process_image))
-                .route(web::get().to(index)),
-        )
+        App::new()
+            .wrap(
+                Cors::new() // <- Construct CORS middleware builder
+                    .allowed_origin("https://www.lupusanay.me")
+                    .allowed_origin("https://lupusanay.me")
+                    .allowed_origin("http://localhost:8080")
+                    .allowed_origin("https://localhost:8080")
+                    .allowed_origin("http://0.0.0.0:8080")
+                    .allowed_origin("https://0.0.0.0:8080")
+                    .allowed_origin("http://localhost:8000")
+                    .allowed_origin("https://localhost:8000")
+                    .allowed_origin("http://0.0.0.0:8000")
+                    .allowed_origin("https://0.0.0.0:8000")
+                    .allowed_methods(vec!["GET", "POST", "PATCH", "PUT", "DELETE"])
+                    .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                    .allowed_header(http::header::CONTENT_TYPE))
+            .service(
+                web::resource("/image")
+                    .route(web::post().to_async(process_image))
+                    .route(web::get().to(index)),
+            )
     })
         .bind("127.0.0.1:8080")?
         .run()

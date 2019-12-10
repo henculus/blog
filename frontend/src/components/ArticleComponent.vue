36cd4e836db46d65650ce7e7bcc86f0e47da0d61
SarielAki <sariely@yandex.ru>
2019-12-10 22:13:21 +0300

    Added parsing text and images from delta format
    #158

--- frontend/src/components/ArticleComponent.vue
+++ frontend/src/components/ArticleComponent.vue
@@ -13,11 +13,22 @@
                         <div class="subtitle">{{article.subtitle}}</div>
                         <div class="author">{{article.author}}</div>
                     </section>
-                    <section class="article-content">
-                        <p class="paragraph">{{article.body}}</p>
+                    <section class="article-content"
+                             v-for="(element, key) in articleBody.ops"
+                             :key="key"
+                    >
+                        <p class="paragraph"
+                           v-if="!element.insert.hasOwnProperty('lazyImage')"
+                           v-html="element.insert.replace(/\n/g, `<br />`)"
+                        >
+                        </p>
+                        <lazy-image
+                                v-if="element.insert.hasOwnProperty('lazyImage')"
+                                :img-padding="56.25"
+                                :low-res-img-path="element.insert.lazyImage.lowResUrl"
+                                :high-res-img-path="element.insert.lazyImage.highResUrl"
+                        ></lazy-image>
                     </section>
-                    <lazy-image :img-padding="56.25" :low-res-img-path="`https://i.imgur.com/L5yPU0P.jpg`"
-                                :high-res-img-path="`https://i.imgur.com/uingZZO.jpg`"></lazy-image>
                 </article>
 
             </div>
@@ -40,6 +51,11 @@
             return {
                 article: {},
                 isLoading: true,
+            }
+        },
+        computed: {
+            articleBody : function () {
+                return JSON.parse(this.article.body)
             }
         },
         mounted: function () {
